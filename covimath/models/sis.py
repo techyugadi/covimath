32ca2f21275b62d556b7c1e758d57932af8adc17
techyugadi <techyugadi@gmail.com>
2021-06-24 16:39:54 -0400

    added code in terms of absolute numbers

--- covimath/models/sis.py
+++ covimath/models/sis.py
@@ -9,18 +9,18 @@ logger = logging.getLogger(__name__)
 
 class SIS:
     
-    def __init__(self, N, lam, mu, gamma, S0, I0, tau):
+    def __init__(self, N, lam, mu, gamma, I0, tau):
         logger.info('Intializing SIS model ...')
         self.N = N # Total population (assumed constant)
         self.lam = lam # Daily contact rate (for adequate contact)
         self.mu = mu # Proportionality constant of daily deaths (in each class)
         self.gamma = gamma # Proportionality constant of daily recovery
-        self.S0 = S0 # Initial susceptible poopulation as fraction of N
-        self.I0 = I0 # Initial infected population as fraction of N
+        self.I0 = I0 # Initial infected population (number, not fraction)
+        self.S0 = N - I0 # Initial susceptible population (number, not fraction)
         self.tau = tau # Time window in days over which to model
         self.sigma = lam * 1.0 / (gamma + mu) # contact number
     
-    # Closed form function : Infected population
+    # Closed form function : Infected population (fraction)
     def i(self, t):
         lam = self.lam
         gamma = self.gamma
@@ -37,26 +37,54 @@ class SIS:
             
             return num * 1.0 / den
     
-    # Closed form function : Susceptibile poputation
+    # Infected population (absolute number)
+    def iabs(self, t):
+        return self.i(t) * self.N
+    
+    # Closed form function : Susceptibile poputation (fraction)
     def s(self, t):
         return 1.0 - self.i(t)
     
-    def plot(self):
+    # Susceptible population (absolute number)
+    def sabs(self, t):
+        return self.s(t) * self.N
+    
+    def plot(self, absolute=False):
         logger.info('Plotting SIS model ...')
         T = np.linspace(0, self.tau, self.tau)
-        I = [self.i(tim) for tim in T]
-        S = [self.s(tim) for tim in T]
+        N = self.N
+        
+        if absolute:
+            I = [self.iabs(tim) for tim in T]
+            S = [self.sabs(tim) for tim in T]
+        else:
+            I = [self.i(tim) for tim in T]
+            S = [self.s(tim) for tim in T]
         
         Iarr = np.array(I)
         Sarr = np.array(S)
         
         fig = plt.figure()
         ax = fig.add_subplot(111)
-        ax.plot(T, Sarr, label='Susceptible Fraction')
-        ax.plot(T, Iarr, label='Infected Fraction')
+        
+        if absolute:
+            slab = 'Susceptible Population'
+            ilab = 'Infected Population'
+        else:
+            slab = 'Susceptible Fraction'
+            ilab = 'Infected Fraction'
+            
+        ax.plot(T, Sarr, label=slab)
+        ax.plot(T, Iarr, label=ilab)
+        
         ax.set_xlabel('Days')
-        ax.set_ylabel('Fraction of Population')
-        ax.set_ylim(0, 1.1)
+        if absolute:
+            ax.set_ylabel('Number of People')
+            ax.set_ylim(0, N+100)
+        else:
+            ax.set_ylabel('Fraction of Population')
+            ax.set_ylim(0, 1.1)
+            
         legend = ax.legend()
         
         plt.show()
@@ -64,12 +92,12 @@ class SIS:
 if __name__ == "__main__":
 
     model = SIS(N=1000, lam=0.05, mu = 0.15, 
-                gamma=1./10, S0=999, I0=1, tau=150)
+                gamma=1./10, I0=1, tau=30)
     
     print(model.i(20))
     print(model.s(20))
     
-    model.plot()
+    model.plot(absolute=True)
         
 
 
--- covimath/tests/test_sis.py
+++ covimath/tests/test_sis.py
@@ -3,12 +3,12 @@ from covimath.models import sis
 
 def test_inf():
     model = sis.SIS(N=1000, lam=0.05, mu = 0.15, 
-                gamma=1./10, S0=999, I0=1, tau=150)
+                gamma=1./10, I0=1, tau=150)
     i = model.i(20) # should be around 1.8%
     assert ceil(i * 100) == 2.0
 
 def test_sus():
     model = sis.SIS(N=1000, lam=0.05, mu = 0.15, 
-                gamma=1./10, S0=999, I0=1, tau=150)
+                gamma=1./10, I0=1, tau=150)
     s = model.s(20) # should be around 98%
     assert floor(s * 100) == 98
