bf6fcd6a4d44c12c37da05c3036ce04faa2b4fe1
techyugadi <techyugadi@gmail.com>
2021-06-28 11:11:30 -0400

    Created package files

---
+++ covimath/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+PKG='covimath'
+VER='0.1.0'
+def main():
+    print(PKG + ' ' + VER)
+    
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import find_packages, setup
+
+cd = pathlib.Path(__file__).parent
+
+# The text of the README file
+readme = (cd / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="covimath",
+    packages=find_packages(exclude=("tests",)),
+    version="0.1.0",
+    description="Some mathematical models on epidemiology / Covid-19 infections",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/techyugadi/covimath",
+    download_url='https://github.com/techyugadi/covimath/archive/refs/tags/v0.1.0.tar.gz',
+    author="TechYugadi",
+    author_email="techyugadi@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    install_requires=["numpy", "scipy", "matplotlib", "pytest"],
+    entry_points={
+        "console_scripts": [
+            "covimath=covimath.__main__:main",
+        ]
+    },
+)
